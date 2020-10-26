2a72b03bfc32573b0cd2a04c146b96a0a6641358
Rizqi Wijaya <rizwijaya@github.com>
2020-10-26 11:36:16 +0700

    Tambah, Update, Delete pada Data Mobil

--- app/controllers/Mobil.php
+++ app/controllers/Mobil.php
@@ -35,20 +35,73 @@ class mobil extends Controller
         if ($size < 2048000 and ($type =='image/jpeg' or $type == 'image/png' or $type == 'image/jpg')) {
             move_uploaded_file($temp, $folder . $name); //Melakukan upload ke folder/nama
             //Input ke database
-            if ($this->model('Rental_model')->tambah_mobil($_POST, $name) > 0) {
-                Flasher::setFlash('Data Kendaraan berhasil', ' ditambahkan', 'success');
+            if ($this->model('Rental_model')->tambah_mobil($_POST, $name) > 0) {    //Menambahkan ke database
+                Flasher::setFlash_modal('Data Kendaraan telah berhasil ditambahkan.', 'Data Kendaraan Ditambahkan!', 'success');
                     header('location: ' . BASEURL . '/mobil/data_mobil');
                     exit;
                 } else {
-                    Flasher::setFlash('Data Kendaraan gagal', ' ditambahkan', 'danger');
+                    Flasher::setFlash_modal('Kesalahan, data Kendaraan gagal untuk ditambahkan.', 'Data Kendaraan gagal ditambahkan!', 'danger');
                     header('location: ' . BASEURL . '/mobil/data_mobil');
                     exit;
-                }
+                } 
         }else{
-            Flasher::setFlash('gagal', ' Upload Gambar', 'danger');
+            Flasher::setFlash_modal('Kesalahan, data Kendaraan gagal untuk ditambahkan.', 'Data Kendaraan gagal ditambahkan!', 'danger');
             header('location: ' . BASEURL . '/mobil/data_mobil');
             exit;
         }
+    }
+
+    public function detail($id)
+    {
+
+    }
+
+    public function delete($id)
+    {
+        //Ambil data mobil dari database gambar, selanjutnya hapus
+        if ($this->model('rental_model')->hapusDataMobil($id) > 0) {
+        
+            Flasher::setFlash_modal('Data Kendaraan telah berhasil dihapus.', 'Data Kendaraan Dihapus!', 'success');
+            header('location: ' . BASEURL . '/mobil/data_mobil');
+            exit;
+        } else {
+            Flasher::setFlash_modal('Kesalahan, data Kendaraan gagal untuk dihapus.', 'Data Kendaraan gagal gagas dihapus!', 'danger');
+            header('location: ' . BASEURL . '/mobil/data_mobil');
+            exit;
+        }
+    }
+
+    public function update()
+    {
+        $temp = $_FILES['gambar']['tmp_name'];
+        if($temp) {
+            //Inisialisasi Data Gambar
+            $name = rand(0,9999).$_FILES['gambar']['name'];
+            $size = $_FILES['gambar']['size'];
+            $type = $_FILES['gambar']['type'];
+            $folder = "foto_mobil/";
+            //Melakukan pengecekan ukuran file dan format
+            if ($size < 2048000 and ($type =='image/jpeg' or $type == 'image/png' or $type == 'image/jpg')) {
+                move_uploaded_file($temp, $folder . $name); //Melakukan upload foto ke folder/nama
+                $this->model('Rental_model')->update_foto_mobil($name, $_POST); //Input ke database foto mobil
+                //Notifikasi sukses update
+                Flasher::setFlash_modal('Data Kendaraan telah berhasil diperbarui.', 'Data Kendaraan Diperbarui!', 'success');
+                //header('location: ' . BASEURL . '/mobil/data_mobil');
+                exit;
+            }
+            else{
+                Flasher::setFlash_modal('Kesalahan, data Kendaraan gagal untuk diperbarui.', 'Data Kendaraan gagal ditambahkan!', 'danger');
+                header('location: ' . BASEURL . '/mobil/data_mobil');
+                exit;
+            } 
+        }
+        //Input ke database
+        if ($this->model('Rental_model')->update_mobil($_POST)) {
+            Flasher::setFlash_modal('Data Kendaraan telah berhasil diperbarui.', 'Data Kendaraan Diperbarui!', 'success');
+            header('location: ' . BASEURL . '/mobil/data_mobil');
+            exit;
+        }
+        header('location: ' . BASEURL . '/mobil/data_mobil');
     }
 /*
     public function data_type()
--- app/models/Rental_model.php
+++ app/models/Rental_model.php
@@ -39,4 +39,56 @@ class Rental_model {
 
         return $this->db->rowCount();
     }
+
+    function update($id)
+    {
+        $q = "SELECT * FROM mobil mb, type tp WHERE mb.kode_type = tp.kode_type AND mb.id_mobil ='$id'";
+        $this->db->query($q);
+        return $this->db->resultSet();
+    }
+
+    public function update_mobil($data)
+    {
+        $query = "UPDATE mobil SET
+                    kode_type = :kode_type, 
+                    merk = :merk, 
+                    no_plat = :no_plat, 
+                    warna = :warna,
+                    tahun = :tahun, 
+                    status = :status
+                WHERE id_mobil= :id_mobil";
+
+        $this->db->query($query);
+        $this->db->bind('kode_type', $data['kode_type']);
+        $this->db->bind('merk', $data['merk']);
+        $this->db->bind('no_plat', $data['no_plat']);
+        $this->db->bind('warna', $data['warna']);
+        $this->db->bind('tahun', $data['tahun']);
+        $this->db->bind('status', $data['status']);
+        $this->db->bind('id_mobil', $data['id_mobil']);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
+
+    public function update_foto_mobil($name, $data)
+    {
+        $this->db->query("UPDATE mobil SET gambar = :gambar WHERE id_mobil= :id_mobil");
+        $this->db->bind('gambar', $name);
+        $this->db->bind('id_mobil', $data['id_mobil']);
+        $this->db->execute();
+        return $this->db->rowCount();
+    }
+
+    public function hapusDataMobil($id)
+    {
+        $query = "DELETE FROM mobil WHERE id_mobil = :id_mobil";
+        $this->db->query($query);
+        $this->db->bind('id_mobil', $id);
+
+        $this->db->execute();
+
+        return $this->db->rowCount();
+    }
 }
